1ee1a8fc580702aa34a5a203dc15c43607f3e559
ebyy12 <148988993+ebyy12@users.noreply.github.com>
2024-01-02 12:42:24 +0700

    Add files via upload

---
+++ stream.py
@@ -0,0 +1,79 @@
+import streamlit as st
+import pandas as pd
+from mlxtend.frequent_patterns import association_rules, apriori
+
+df = pd.read_csv("Groceries_dataset.csv")
+
+df['Date'] = pd.to_datetime(df['Date'], format="%d-%m-%Y")
+
+df["month"] = df['Date'].dt.month
+df["day"] = df['Date'].dt.weekday
+
+df["month"].replace([i for i in range(1, 12 + 1)], ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"], inplace=True)
+df["day"].replace([i for i in range(6 + 1)], ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu'], inplace=True)
+
+st.title("Market Basket Analysis")
+
+def get_data(month='', day=''):
+    data = df.copy()
+    filtered = data.loc[
+        (data["month"].str.contains(month.title())) &
+        (data["day"].str.contains(day.title()))
+    ]
+    return filtered if not filtered.empty else "No result"
+
+def user_input_feature():
+    item_list = df['itemDescription'].unique().tolist()
+    item = st.selectbox("Item", item_list)
+    month = st.select_slider("Month", ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"])
+    day = st.select_slider("Day", ['Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab', 'Min'], value="Sen")
+
+    return item, month, day
+
+month, day, item = user_input_feature()
+
+data = get_data(month, day)
+
+def encode(x):
+    if x <= 0:
+        return 0
+    elif x >= 1:
+        return 1
+
+if type(data) != type("No Result"):
+    item_count = df.groupby(["Member_number", "itemDescription"])["itemDescription"].count().reset_index(name="Count")
+    item_count_pivot = item_count.pivot_table(index='Member_number', columns='itemDescription', values='Count', aggfunc='sum').fillna(0)
+    item_count_pivot = item_count_pivot.applymap(encode)
+
+    support = 0.01
+    frequent_items = apriori(item_count_pivot, min_support=support, use_colnames=True)
+
+    metric = "lift"
+    min_treshold = 1
+
+    rules = association_rules(frequent_items, metric=metric, min_threshold=min_treshold)[["antecedents", "consequents", "support", "confidence", "lift"]]
+    rules.sort_values('confidence', ascending=False, inplace=True)
+if 'rules' in locals():
+    st.write("Generated Association Rules:")
+    st.write(rules)
+else:
+    st.warning("No rules generated. Please adjust your input criteria.")
+
+def parse_list(x):
+    x = list(x)
+    if len(x) == 1:
+        return x[0]
+    elif len(x) > 1:
+        return ", ".join(x)
+
+def return_item_df(item_antecedents):
+    data = rules[["antecedents", "consequents"]].copy()
+
+    data["antecedents"] = data["antecedents"].apply(parse_list)
+    data["consequents"] = data["consequents"].apply(parse_list)
+
+    return list(data.loc[data["antecedents"] == item_antecedents].iloc[0, :])
+
+if type(data) != type("No result"):
+    st.markdown("Hasil Rekomendasi : ")
+    st.success(f"Jika konsumen membeli **{item}**, maka membeli **{return_item_df(item)[1]}** secara bersamaan")
